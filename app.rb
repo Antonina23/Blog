f92d53ea74e9bd9e443cf08ffded334718b59eb6
Antonina23 <tone4kawow@gmail.com>
2018-11-06 21:39:15 +0300

    Add index erb

--- app.rb
+++ app.rb
@@ -22,7 +22,7 @@ configure do
 end
 
 get '/' do
-	erb "Hello,there! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
+	erb :index		
 end
 
 get '/new' do
@@ -38,7 +38,6 @@ post '/new' do
   end
 
 @db.execute 'insert into Posts (content, created_date) values (?, datetime())', [content]
-
 
   erb "You typed: \n#{content}"
 end
